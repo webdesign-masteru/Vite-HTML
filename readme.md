694a01ce8279c2bfc2b99997c770f6045e507e4e
Алексей <agragregra@ya.ru>
2025-02-09 16:22:02 +0300

    Update

--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 <p>Lightweight Vite startup template for a traditional multi-page websites.</p>
 
 <p>
-	<img src="https://raw.githubusercontent.com/agragregra/Vite-HTML/master/src/assets/img/example/preview.jpg" alt="Start HTML Template">
+  <img src="https://raw.githubusercontent.com/agragregra/Vite-HTML/master/src/assets/img/example/preview.jpg" alt="Start HTML Template">
 </p>
 
 <h2>How to use Vite HTML</h2>
@@ -13,9 +13,9 @@
 <pre>git clone https://github.com/agragregra/vh .; rm -rf trunk .gitignore .git readme.md</pre>
 
 <ol>
-	<li>Clone or <a href="https://github.com/agragregra/Vite-HTML/archive/master.zip">Download</a> <strong>Vite HTML</strong> from GitHub</li>
-	<li>Install мodules: <strong>npm i</strong></li>
-	<li>Run: <strong>npm run dev</strong></li>
+  <li>Clone or <a href="https://github.com/agragregra/Vite-HTML/archive/master.zip">Download</a> <strong>Vite HTML</strong> from GitHub</li>
+  <li>Install мodules: <strong>npm i</strong></li>
+  <li>Run: <strong>npm run dev</strong></li>
 </ol>
 
 <blockquote>Important rule! Do not delete the <code>second.html</code> file until the new next file of the multi-page site is created.</blockquote>
--- vite.config.js
+++ vite.config.js
@@ -3,35 +3,35 @@ import { defineConfig } from 'vite'
 import { sync } from 'glob'
 
 const noAttr = () => {
-	return {
-		transformIndexHtml(html) {
-			return html.replaceAll(' crossorigin', '')
-		}
-	}
+  return {
+    transformIndexHtml(html) {
+      return html.replaceAll(' crossorigin', '')
+    }
+  }
 }
 
 export default defineConfig({
-	plugins: [
-		SassGlob(),
-		noAttr(),
-	],
-	build: {
-		rollupOptions: {
-			input: sync('src/**/*.html'.replace(/\\/g, '/')),
-			output: {
-				assetFileNames: (assetInfo) => {
-					let extType = assetInfo.name
-					if (/css/.test(extType)) { extType = 'assets/css' }
-					return assetInfo.originalFileName ?? `${extType}/[name][extname]`
-				},
-				chunkFileNames: 'assets/js/[name].js',
-				entryFileNames: 'assets/js/[name].js',
-			},
-		},
-		assetsInlineLimit: 0,
-		emptyOutDir: true,
-		outDir: '../dist',
-	},
-	root: 'src',
-	base: '',
+  plugins: [
+    SassGlob(),
+    noAttr(),
+  ],
+  build: {
+    rollupOptions: {
+      input: sync('src/**/*.html'.replace(/\\/g, '/')),
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name
+          if (/css/.test(extType)) { extType = 'assets/css' }
+          return assetInfo.originalFileName ?? `${extType}/[name][extname]`
+        },
+        chunkFileNames: 'assets/js/[name].js',
+        entryFileNames: 'assets/js/[name].js',
+      },
+    },
+    assetsInlineLimit: 0,
+    emptyOutDir: true,
+    outDir: '../dist',
+  },
+  root: 'src',
+  base: '',
 })
